7f2b8caac8f0ee6ed535a00b1b0d5176a065a844
berziiii <brian@offpistedesigns.com>
2016-07-31 13:12:36 -0400

    initial commit

---
+++ app/application/adapter.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import ActiveModelAdapter from 'active-model-adapter';
+import ENV from 'tracker-app/config/environment';
+
+export default ActiveModelAdapter.extend({
+  auth: Ember.inject.service(),
+  host: ENV.apiHost,
+  
+  headers: Ember.computed('auth.credentials.token', {
+    get () {
+      let headers = {};
+      const token = this.get('auth.credentials.token');
+      if (token) {
+        headers.Authorization = `Token token=${token}`;
+      }
+
+      return headers;
+    },
+  }),
+});
